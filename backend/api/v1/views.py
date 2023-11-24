65d6d3a4d4b5032f9eb5cd2a9e33b07c986fe4b5
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2023-11-25 01:25:28 +0300

    Update UserViewSet (add get)

--- backend/api/v1/views.py
+++ backend/api/v1/views.py
@@ -1,10 +1,13 @@
 from drf_spectacular.utils import extend_schema, extend_schema_view
+from rest_framework.permissions import IsAdminUser
 from rest_framework.viewsets import ModelViewSet
 from rest_framework_simplejwt.views import (
     TokenObtainPairView, TokenRefreshView,
 )
 
-from api.v1.serializers import NewsSerializer, UserRegisterSerializer
+from api.v1.serializers import (
+    NewsSerializer, UserFullSerializer, UserRegisterSerializer,
+)
 from api.v1.schemas_views import (
     NEWS_SCHEMA, TOKEN_OBTAIN_SCHEMA, TOKEN_REFRESH_SCHEMA, USERS_SCHEMA,
 )
@@ -38,16 +41,19 @@ class NewsViewSet(ModelViewSet):
     serializer_class = NewsSerializer
 
 
-
 @extend_schema_view(**USERS_SCHEMA)
 class UserViewSet(ModelViewSet):
     """ViewSet для взаимодействия с моделью User."""
 
-    http_method_names = ('post',)
-    # http_method_names = ('get', 'post', 'patch',)
+    http_method_names = ('get', 'post',)
     queryset = User.objects.select_related('address',).all()
 
     def get_serializer_class(self):
-        if self.request.method == 'post':
+        if self.request.method == 'POST':
             return UserRegisterSerializer
-        return UserRegisterSerializer
+        return UserFullSerializer
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            self.permission_classes = [IsAdminUser,]
+        return super().get_permissions()
