3ec1bf24629b5f457e9cfed6f57e327ed31c00b8
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2023-11-24 22:27:48 +0300

    Init info app

---
+++ backend/info/admin.py
@@ -0,0 +1,391 @@
+from django.contrib import admin
+
+from info.models import (
+    Answer, AnswerUser, Appeal,
+    News, NewsCategory, NewsComment, NewsPicture,
+    Task, Quiz,
+)
+from urban_utopia_2024.app_data import ADMIN_LIST_PER_PAGE
+
+
+@admin.register(Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    """
+    Переопределяет административный интерфейс Django для модели Answer.
+
+    Атрибуты:
+        - list_display (tuple) - список полей для отображения в интерфейсе:
+            - ID ответа (id)
+            - ID опроса (quiz)
+            - текст ответа (text)
+        - list_editable (tuple) - список полей для изменения в интерфейсе:
+            - ID опроса (quiz)
+            - текст ответа (text)
+        - list_filter (tuple) - список фильтров:
+            - ID опроса (quiz)
+        - list_per_page (int) - количество объектов на одной странице
+    """
+    list_display = (
+        'id',
+        'quiz',
+        'text',
+    )
+    list_editable = (
+        'quiz',
+        'text',
+    )
+    list_filter = (
+        'quiz',
+    )
+    list_per_page = ADMIN_LIST_PER_PAGE
+
+
+@admin.register(AnswerUser)
+class AnswerUserAdmin(admin.ModelAdmin):
+    """
+    Переопределяет административный интерфейс Django для модели AnswerUser.
+
+    Атрибуты:
+        - list_display (tuple) - список полей для отображения в интерфейсе:
+            - ID ответа пользователя (id)
+            - ID ответа (answer)
+            - ID пользователя (user)
+        - list_editable (tuple) - список полей для изменения в интерфейсе:
+            - ID ответа (answer)
+            - ID пользователя (user)
+        - list_filter (tuple) - список фильтров:
+            - ID ответа (answer)
+            - ID пользователя (user)
+        - list_per_page (int) - количество объектов на одной странице
+    """
+    list_display = (
+        'id',
+        'answer',
+        'user',
+    )
+    list_editable = (
+        'answer',
+        'user',
+    )
+    list_filter = (
+        'answer',
+        'user',
+    )
+    list_per_page = ADMIN_LIST_PER_PAGE
+
+
+@admin.register(Appeal)
+class AppealAdmin(admin.ModelAdmin):
+    """
+    Переопределяет административный интерфейс Django для модели Appeal.
+
+    Атрибуты:
+        - list_display (tuple) - список полей для отображения в интерфейсе:
+            - ID обращения (id)
+            - ID пользователя (user)
+            - тема обращения (topic)
+            - текст обращения (text)
+            - дата и время обращения (pub_date)
+            - ID адреса (address)
+            - текст ответа на обращение (answer)
+            - статус обращения (status)
+            - рейтинг ответа обращения (rating)
+        - list_editable (tuple) - список полей для изменения в интерфейсе:
+            - ID пользователя (user)
+            - тема обращения (topic)
+            - текст обращения (text)
+            - ID адреса (address)
+            - текст ответа на обращение (answer)
+            - статус обращения (status)
+            - рейтинг ответа обращения (rating)
+        - list_filter (tuple) - список фильтров:
+            - ID пользователя (user)
+            - ID адреса (address)
+            - статус обращения (status)
+            - рейтинг ответа обращения (rating)
+        - search_fields (tuple) - список полей для поиска объектов:
+            - тема обращения (topic)
+            - текст обращения (text)
+            - дата и время обращения (pub_date)
+            - текст ответа на обращение (answer)
+        - list_per_page (int) - количество объектов на одной странице
+    """
+    list_display = (
+        'id',
+        'user',
+        'topic',
+        'text',
+        'pub_date',
+        'address',
+        'answer',
+        'status',
+        'rating',
+    )
+    list_editable = (
+        'user',
+        'topic',
+        'text',
+        'address',
+        'answer',
+        'status',
+        'rating',
+    )
+    list_filter = (
+        'user',
+        'address',
+        'status',
+        'rating',
+    )
+    search_fields = (
+        'topic',
+        'text',
+        'pub_date',
+        'answer',
+    )
+    list_per_page = ADMIN_LIST_PER_PAGE
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    """
+    Переопределяет административный интерфейс Django для модели News.
+
+    Атрибуты:
+        - list_display (tuple) - список полей для отображения в интерфейсе:
+            - ID новости (id)
+            - категория (category)
+            - текст (text)
+            - ID адреса (address)
+            - дата и время обращения (pub_date)
+            - ID опроса (quiz)
+        - list_editable (tuple) - список полей для изменения в интерфейсе:
+            - категория (category)
+            - текст (text)
+            - ID адреса (address)
+            - ID опроса (quiz)
+        - list_filter (tuple) - список фильтров:
+            - категория (category)
+            - ID адреса (address)
+        - search_fields (tuple) - список полей для поиска объектов:
+            - текст (text)
+            - дата и время обращения (pub_date)
+            - ID опроса (quiz)
+        - list_per_page (int) - количество объектов на одной странице
+    """
+    list_display = (
+        'id',
+        'category',
+        'text',
+        'address',
+        'pub_date',
+        'quiz',
+    )
+    list_editable = (
+        'category',
+        'text',
+        'address',
+        'quiz',
+    )
+    list_filter = (
+        'category',
+        'address',
+    )
+    search_fields = (
+        'text',
+        'pub_date',
+        'quiz',
+    )
+    list_per_page = ADMIN_LIST_PER_PAGE
+
+
+@admin.register(NewsCategory)
+class NewsCategoryAdmin(admin.ModelAdmin):
+    """
+    Переопределяет административный интерфейс Django для модели NewsCategory.
+
+    Атрибуты:
+        - list_display (tuple) - список полей для отображения в интерфейсе:
+            - ID категории новостей (id)
+            - название категории (name)
+        - list_editable (tuple) - список полей для изменения в интерфейсе:
+            - название категории (name)
+        - search_fields (tuple) - список полей для поиска объектов:
+            - название категории (name)
+        - list_per_page (int) - количество объектов на одной странице
+    """
+    list_display = (
+        'id',
+        'name',
+    )
+    list_editable = (
+        'name',
+    )
+    search_fields = (
+        'name',
+    )
+    list_per_page = ADMIN_LIST_PER_PAGE
+
+
+@admin.register(NewsComment)
+class NewsCommentAdmin(admin.ModelAdmin):
+    """
+    Переопределяет административный интерфейс Django для модели NewsComment.
+
+    Атрибуты:
+        - list_display (tuple) - список полей для отображения в интерфейсе:
+            - ID комментария новости (id)
+            - ID новости (news)
+            - текст комментария (comment)
+            - дата и время публикации (pub_date)
+        - list_editable (tuple) - список полей для изменения в интерфейсе:
+            - ID новости (news)
+            - текст комментария (comment)
+        - list_filter (tuple) - список фильтров:
+            - ID новости (news)
+        - search_fields (tuple) - список полей для поиска объектов:
+            - текст комментария (comment)
+            - дата и время публикации (pub_date)
+        - list_per_page (int) - количество объектов на одной странице
+    """
+    list_display = (
+        'id',
+        'news',
+        'comment',
+        'pub_date',
+    )
+    list_editable = (
+        'news',
+        'comment',
+    )
+    list_filter = (
+        'news',
+    )
+    search_fields = (
+        'comment',
+        'pub_date',
+    )
+    list_per_page = ADMIN_LIST_PER_PAGE
+
+
+@admin.register(NewsPicture)
+class NewsPictureAdmin(admin.ModelAdmin):
+    """
+    Переопределяет административный интерфейс Django для модели NewsPicture.
+
+    Атрибуты:
+        - list_display (tuple) - список полей для отображения в интерфейсе:
+            - ID картинки новости (id)
+            - ID новости (news)
+            - картинка (picture)
+        - list_editable (tuple) - список полей для изменения в интерфейсе:
+            - ID новости (news)
+            - картинка (picture)
+        - list_filter (tuple) - список фильтров:
+            - ID новости (news)
+        - search_fields (tuple) - список полей для поиска объектов:
+            - картинка (picture)
+        - list_per_page (int) - количество объектов на одной странице
+    """
+    list_display = (
+        'id',
+        'news',
+        'picture',
+    )
+    list_editable = (
+        'news',
+        'picture',
+    )
+    list_filter = (
+        'news',
+    )
+    search_fields = (
+        'picture',
+    )
+    list_per_page = ADMIN_LIST_PER_PAGE
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    """
+    Переопределяет административный интерфейс Django для модели Task.
+
+    Атрибуты:
+        - list_display (tuple) - список полей для отображения в интерфейсе:
+            - ID задачи (id)
+            - заголовок (title)
+            - ID муниципальной службы (municipal)
+            - ID адреса (address)
+            - дата и время обращения (pub_date)
+            - дата и время начала работ (start_date)
+            - дата и время окончания работ (end_date)
+        - list_editable (tuple) - список полей для изменения в интерфейсе:
+            - заголовок (title)
+            - ID муниципальной службы (municipal)
+            - ID адреса (address)
+            - дата и время начала работ (start_date)
+            - дата и время окончания работ (end_date)
+        - list_filter (tuple) - список фильтров:
+            - ID муниципальной службы (municipal)
+            - ID адреса (address)
+        - search_fields (tuple) - список полей для поиска объектов:
+            - заголовок (title)
+            - дата и время обращения (pub_date)
+            - дата и время начала работ (start_date)
+            - дата и время окончания работ (end_date)
+        - list_per_page (int) - количество объектов на одной странице
+    """
+    list_display = (
+        'id',
+        'title',
+        'municipal',
+        'address',
+        'pub_date',
+        'start_date',
+        'end_date',
+    )
+    list_editable = (
+        'title',
+        'municipal',
+        'address',
+        'start_date',
+        'end_date',
+    )
+    list_filter = (
+        'municipal',
+        'address',
+    )
+    search_fields = (
+        'title',
+        'pub_date',
+        'start_date',
+        'end_date',
+    )
+    list_per_page = ADMIN_LIST_PER_PAGE
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    """
+    Переопределяет административный интерфейс Django для модели Quiz.
+
+    Атрибуты:
+        - list_display (tuple) - список полей для отображения в интерфейсе:
+            - ID опроса (id)
+            - заголовок (title)
+        - list_editable (tuple) - список полей для изменения в интерфейсе:
+            - заголовок (title)
+        - search_fields (tuple) - список полей для поиска объектов:
+            - заголовок (title)
+        - list_per_page (int) - количество объектов на одной странице
+    """
+    list_display = (
+        'id',
+        'title',
+    )
+    list_editable = (
+        'title',
+    )
+    search_fields = (
+        'title',
+    )
+    list_per_page = ADMIN_LIST_PER_PAGE
---
+++ backend/info/models.py
@@ -0,0 +1,330 @@
+from django.core.validators import MaxValueValidator
+from django.db import models
+
+from urban_utopia_2024.app_data import (
+    APPEAL_RATING_MAX_VAL, APPEAL_RATING_MESSAGE,
+    APPEAL_STATUS_CHOICES, APPEAL_STAGE_INITIAL, APPEAL_STATUS_MAX_LEN,
+    APPEAL_TEXT_MAX_LEN, APPEAL_TOPIC_MAX_LEN,
+    NEWS_CATEGORY_CHOICES, NEWS_CATEGORY_MAX_LEN,
+    NEWS_COMMENT_MAX_LEN, NEWS_COMMENT_SLICE,
+    NEWS_PICTURES_PATH, NEWS_TEXT_MAX_LEN,
+    TASK_TITLE_MAX_LEN,
+    QUIZ_ANSWER_MAX_LEN, QUIZ_ANSWER_SLICE, QUIZ_TITLE_MAX_LEN,
+)
+from user.models import Address, User
+
+
+class Task(models.Model):
+    """Модель плановых работ."""
+
+    title = models.CharField(
+        verbose_name='Заголовок',
+        max_length=TASK_TITLE_MAX_LEN,
+    )
+    municipal = models.ForeignKey(
+        verbose_name='Муниципальная служба',
+        to=User,
+        related_name='task',
+        on_delete=models.PROTECT,
+    )
+    address = models.ForeignKey(
+        verbose_name='Адрес',
+        to=Address,
+        related_name='tasks',
+        on_delete=models.PROTECT,
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        auto_now_add=True,
+    )
+    start_date = models.DateTimeField(
+        verbose_name='Дата и время начала работ',
+    )
+    end_date = models.DateTimeField(
+        verbose_name='Дата и время окончания работ',
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('title', 'municipal'),
+                name='unique_municipal_task',
+            ),
+        )
+        ordering = ('id',)
+        verbose_name = 'Плановая работа'
+        verbose_name_plural = 'Плановые работы'
+
+    def __str__(self):
+        return f'{self.title} ({self.municipal.municipal_name})'
+
+
+class Quiz(models.Model):
+    """Модель опросов."""
+
+    title = models.CharField(
+        verbose_name='Заголовок',
+        max_length=QUIZ_TITLE_MAX_LEN,
+        unique=True,
+    )
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Опрос'
+        verbose_name_plural = 'Опросы'
+
+    def __str__(self):
+        return self.title
+
+
+class Answer(models.Model):
+    """Модель ответов на опросы."""
+
+    quiz = models.ForeignKey(
+        verbose_name='Опрос',
+        to=Quiz,
+        related_name='answer',
+        on_delete=models.CASCADE,
+    )
+    text = models.CharField(
+        verbose_name='Ответ',
+        max_length=QUIZ_ANSWER_MAX_LEN,
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('quiz', 'text'),
+                name='unique_quiz_answer_text',
+            ),
+        )
+        ordering = ('id',)
+        verbose_name = 'Ответ на опрос'
+        verbose_name_plural = 'Ответы на опросы'
+
+    def __str__(self):
+        return f'{self.quiz}: {self.text[:QUIZ_ANSWER_SLICE]}'
+
+
+class AnswerUser(models.Model):
+    """Модель учета ответивших на вопрос опроса."""
+
+    answer = models.ForeignKey(
+        verbose_name='Ответ на опрос',
+        to=Answer,
+        related_name='answer_user',
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        verbose_name='Ответивший пользователь',
+        to=User,
+        related_name='answer_user',
+        on_delete=models.CASCADE,
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('answer', 'user'),
+                name='unique_answer_user',
+            ),
+        )
+        ordering = ('id',)
+        verbose_name = 'Ответ пользователя'
+        verbose_name_plural = 'Ответы пользователей'
+
+    def __str__(self):
+        return f'{self.user}: {self.answer[:QUIZ_ANSWER_SLICE]}'
+
+
+class NewsCategory(models.Model):
+    """Модель категорий новостей."""
+
+    name = models.CharField(
+        verbose_name='Название',
+        max_length=NEWS_CATEGORY_MAX_LEN,
+        choices=NEWS_CATEGORY_CHOICES,
+        unique=True,
+    )
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Категория новостей'
+        verbose_name_plural = 'Категории новостей'
+
+    def __str__(self):
+        return self.name
+
+
+class News(models.Model):
+    """Модель новостей."""
+
+    category = models.ForeignKey(
+        verbose_name='Категория',
+        to=NewsCategory,
+        related_name='news',
+        on_delete=models.PROTECT,
+    )
+    text = models.TextField(
+        verbose_name='Текст',
+        max_length=NEWS_TEXT_MAX_LEN,
+        unique=True,
+    )
+    address = models.ForeignKey(
+        verbose_name='Адрес',
+        to=Address,
+        related_name='news',
+        on_delete=models.PROTECT,
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        auto_now_add=True,
+    )
+    quiz = models.ForeignKey(
+        verbose_name='Опрос',
+        to=Quiz,
+        related_name='news',
+        on_delete=models.PROTECT,
+    )
+
+    class Meta:
+        ordering = ('id',)
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
+
+    def __str__(self):
+        return f'{self.category} ({self.pub_date})'
+
+
+class NewsPicture(models.Model):
+    """Класс картинок в новостях."""
+
+    news = models.ForeignKey(
+        verbose_name='Новость',
+        to=News,
+        related_name='picture',
+        on_delete=models.CASCADE,
+    )
+    picture = models.ImageField(
+        verbose_name='Картинка',
+        upload_to=NEWS_PICTURES_PATH,
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('news', 'picture'),
+                name='unique_news_picture',
+            ),
+        )
+        ordering = ('news', 'id',)
+        verbose_name = 'Картинка новости'
+        verbose_name_plural = 'Картинки новостей'
+
+    def __str__(self):
+        return f'{self.picture} ({self.news})'
+
+
+class NewsComment(models.Model):
+    """Класс комментариев к новостям."""
+
+    news = models.ForeignKey(
+        verbose_name='Новость',
+        to=News,
+        related_name='comment',
+        on_delete=models.CASCADE,
+    )
+    comment = models.CharField(
+        verbose_name='Комментарий',
+        max_length=NEWS_COMMENT_MAX_LEN,
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        auto_now_add=True,
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('news', 'comment', 'pub_date'),
+                name='unique_single_comment',
+            ),
+        )
+        ordering = ('pub_date',)
+        verbose_name = 'Комментарий новости'
+        verbose_name_plural = 'Комментарии новостей'
+
+    def __str__(self):
+        return f'{self.comment[:NEWS_COMMENT_SLICE]} ({self.news})'
+
+
+class Appeal(models.Model):
+    """Модель обращений граждан."""
+
+    user = models.ForeignKey(
+        verbose_name='Гражданин',
+        to=User,
+        related_name='appeal',
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    topic = models.CharField(
+        verbose_name='Тема',
+        max_length=APPEAL_TOPIC_MAX_LEN,
+    )
+    text = models.TextField(
+        verbose_name='Текст',
+        max_length=APPEAL_TEXT_MAX_LEN,
+    )
+    pub_date = models.DateTimeField(
+        verbose_name='Дата и время публикации',
+        auto_now_add=True,
+    )
+    address = models.ForeignKey(
+        verbose_name='Адрес',
+        to=Address,
+        related_name='appeal',
+        on_delete=models.PROTECT,
+        default=None,
+        blank=True,
+        null=True,
+    )
+    answer = models.TextField(
+        verbose_name='Ответ',
+        max_length=APPEAL_TEXT_MAX_LEN,
+        default=None,
+        blank=True,
+        null=True,
+    )
+    status = models.CharField(
+        verbose_name='Статус ответа',
+        max_length=APPEAL_STATUS_MAX_LEN,
+        choices=APPEAL_STATUS_CHOICES,
+        default=APPEAL_STAGE_INITIAL,
+    )
+    rating = models.PositiveSmallIntegerField(
+        verbose_name='Оценка ответа',
+        validators=(
+            MaxValueValidator(
+                limit_value=APPEAL_RATING_MAX_VAL,
+                message=APPEAL_RATING_MESSAGE,
+            ),
+        ),
+        default=None,
+        blank=True,
+        null=True,
+    )
+
+    class Meta:
+        constraints = (
+            models.UniqueConstraint(
+                fields=('user', 'text'),
+                name='unique_user_text',
+            ),
+        )
+        ordering = ('id',)
+        verbose_name = 'Обращение гражданина'
+        verbose_name_plural = 'Обращение граждан'
+
+    def __str__(self):
+        return f'{self.user} ({self.pub_date})'
