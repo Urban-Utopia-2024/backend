7d5d83eeecfddd2f5c4099b362781ac77ed5ca09
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2023-11-25 14:51:27 +0300

    Add IsMunicipal

---
+++ backend/api/v1/permissions.py
@@ -0,0 +1,8 @@
+from rest_framework import permissions
+
+
+class IsMunicipal(permissions.BasePermission):
+    """Предоставляет доступ только муниципальной службе."""
+
+    def has_permission(self, request, view):
+        return request.user.is_municipal
