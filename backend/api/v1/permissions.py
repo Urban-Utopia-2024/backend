5287aaa341fbf75e6deb414056b2080c68ef0a79
TheSuncatcher222 <TheSuncatcher222@gmail.com>
2023-11-26 13:52:08 +0300

    Fix IsMunicipal

--- backend/api/v1/permissions.py
+++ backend/api/v1/permissions.py
@@ -5,4 +5,4 @@ class IsMunicipal(permissions.BasePermission):
     """Предоставляет доступ только муниципальной службе."""
 
     def has_permission(self, request, view):
-        return request.user.is_municipal
+        return request.user.is_authenticated and request.user.is_municipal
